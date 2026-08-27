9a613189f6a8e4a059594f1a30b3ff6bcc92db36
digiservbiz <digiservbiz@gmail.com>
2026-08-27 11:03:18 +0200

    feat: export production postgres worker integration

--- packages/automation/src/index.ts
+++ packages/automation/src/index.ts
@@ -2,3 +2,5 @@ export * from "./durable-job-queue";
 export * from "./runtime-worker";
 export * from "./scheduled-ingestion";
 export * from "./ingestion-learning-pipeline";
+export * from "./postgres-job-store";
+export * from "./worker-runtime";
